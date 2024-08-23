734998c4d9cf15212720c0ddd1562d7d409a8584
Kirisannn <tanshangian@hotmail.com>
2024-08-24 00:27:01 +0930

    Included Best Solution - Hash Table with int[]

--- ContainsDuplicate/ValidAnagram/topSolution.cpp
+++ ContainsDuplicate/ValidAnagram/topSolution.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 using namespace std;
 
 class Solution {
    public:
     bool isAnagram(string s, string t) {
+        int hash1[26];
+        int hash2[26];
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            hash1[s[i] - 'a']++;
+            hash2[t[i] - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (hash1[i] != hash2[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
